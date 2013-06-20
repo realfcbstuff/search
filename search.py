9abade008ed145b3134542aed4990597492eca57
mikeyfoo <realfcbstuff@yahoo.co.uk>
2013-06-20 16:59:26 +0800

    try commit

--- search.py
+++ search.py
@@ -71,6 +71,9 @@ def tinyMazeSearch(problem):
 
 def depthFirstSearch(problem):
     """
+    search
+    """
+    """
     Search the deepest nodes in the search tree first
 
     Your search algorithm needs to return a list of actions that reaches
